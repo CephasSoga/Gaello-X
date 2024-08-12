1656e791d331e2bb73646f9918a89586ad9350a1
Cephas Nukunu Soga <110126432+CephasSoga@users.noreply.github.com>
2024-08-12 12:02:16 +0000

    updated build.bat (more precise wildcards)

--- build.bat
+++ build.bat
@@ -1,6 +1,16 @@
 @echo off
 pyinstaller --onefile --name Gaello --icon=icon.ico --splash=splash.png ^
-    --add-data "assets/*;assets" ^
+    --add-data "assets/UI/*.ui;assets/UI" ^
+    --add-data "assets/fonts/Exo_2/static/*;assets/fonts/Exo_2/static" ^
+    --add-data "assets/fonts/Montserrat/static/*;assets/fonts/Montserrat/static" ^
+    --add-data "assets/fonts/Quicksand/static/*;assets/fonts/Quicksand/static" ^
+    --add-data "assets/fonts/Roboto_Mono/static/*;assets/fonts/Robot_Mono/static" ^
+    --add-data "assets/images/*;assets/images" ^
+    --add-data "assets/icons/*;assets/icons" ^
+    --add-data "assets/spinners/*;assets/spinners" ^
+    --add-data "assets/videos/*;assets/videos" ^
+    --add-data "assets/html/*;assets/html" ^
+    --add-data "assets/logo/*;assets/logo" ^
     --add-data "node_modules/*;node_modules" ^
     --add-data "server/*;server" ^
     --add-data "views/*;views" ^
